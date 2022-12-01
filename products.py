07fa6e98caf601d3ff63df5bbb00d3ae5bd73c8b
kennychang68 <a0987939193@gmail.com>
2022-12-01 16:04:34 +0800

    add title and define utf-8

--- products.py
+++ products.py
@@ -5,6 +5,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('please type utem price :')
+	price = int(price)
 	##p = []
 	##p.append(name)
 	##p.append(price)
@@ -20,4 +21,10 @@ for p in products:
 	print(p[0], ' price is ', p[1])
 	#print(p[1])
 
+with open('products.csv', 'w', encoding='utf-8' ) as f:
+	#f.write('商品' + ',' + '價格' + '\n')
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+		
 
